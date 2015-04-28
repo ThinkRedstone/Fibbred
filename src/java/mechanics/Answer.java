6558147f6ee46522922decacadd17f817cb44f90
Thinkredstone <ittai11@gmail.com>
2015-04-28 09:22:30 +0300

    Added question and guesser name support in answer

--- src/java/mechanics/Answer.java
+++ src/java/mechanics/Answer.java
@@ -5,12 +5,15 @@
  */
 package mechanics;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author thinkredstone
  */
 public class Answer {
     protected final String answer;
+    protected ArrayList<String> gusssers = new ArrayList<>();
 
     public Answer(String answer) {
         this.answer = answer;
@@ -19,5 +22,17 @@ public class Answer {
     public String getAnswer() {
         return answer;
     }
-     
+
+    public ArrayList<String> getGusssers() {
+        return gusssers;
+    }
+
+    public void setGusssers(ArrayList<String> gusssers) {
+        this.gusssers = gusssers;
+    }
+    
+    public void addGussser(String userName) {
+        this.gusssers.add(userName);
+    }
+         
 }
---
+++ src/java/mechanics/Question.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mechanics;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author thinkredstone
+ */
+public class Question {
+
+    private final Answer rightAnswer;
+    private Set<UserAnswer> userAnswers;
+
+    public Question(Answer rightAnswer, Set<UserAnswer> userAnswers) {
+        this.rightAnswer = rightAnswer;
+        this.userAnswers = userAnswers;
+        checkAnsewers();
+    }
+    
+    public Question(Answer rightAnswer){
+        this.rightAnswer = rightAnswer;
+    }
+    
+    private void checkAnsewers(){
+        for (UserAnswer a : userAnswers) {
+            if (a.answer.equals(rightAnswer.answer)) {
+                this.userAnswers.remove(a);
+            }
+        }
+    }
+
+    public Set<Answer> getAnswers() {
+        HashSet<Answer> answers = new HashSet<>(userAnswers);
+        answers.add(rightAnswer);
+        return answers;
+    }
+
+    public void scoreAnswers(Set<Answer> answers) {
+        for (Answer a : answers) {
+            if (a.getAnswer().equals(rightAnswer.getAnswer())) {
+                rightAnswer.setGusssers(a.getGusssers());
+            } else {
+                for (Answer ua : userAnswers) {
+                    if (ua.answer.equals(a)) {
+                        ua.setGusssers(a.getGusssers());
+                    }
+                }
+            }
+        }
+    }
+}
