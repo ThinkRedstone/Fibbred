c1224542d68aabf414358456541d432059b19a0d
ThinkRedstone <ThinkRedstone@users.noreply.github.com>
2015-05-04 18:15:20 +0300

    Added more documentation.

--- src/java/vars/Vars.java
+++ src/java/vars/Vars.java
@@ -9,12 +9,15 @@ package vars;
 /**
  *
  * @author thinkredstone
+ * all the application and session attributes used 
+ * by the site. Refers to application unless 
+ * specified otherwise.
  */
 public class Vars {
-    public static final String QUESTIONS = "qs";
-    public static final String USER = "user";
-    public static final String CURRENTQ = "q";
-//    public static final String ANSWERS = "a";
-    public static final String AUSERS = "us";//array of all users used to track score
+    public static final String QUESTIONS = "qs";//arraylist of all the questions 
+    public static final String USER = "user";//the session attribute holding the session user
+    public static final String CURRENTQ = "q";//the current question
+//    public static final String ANSWERS = "a";the CURRENTQ question holds all the answers
+    public static final String AUSERS = "us";//arraylist of all users used to track score
     
 }
